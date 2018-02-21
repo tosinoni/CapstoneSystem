26f1e515b6f2d48e029f21fe9e94b4dc06a0f252
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-02-21 11:59:48 -0500

    [Issue #23] Updated dashboard when user logs in

--- src/main/resources/static/js/welcome/welcome-controller.js
+++ src/main/resources/static/js/welcome/welcome-controller.js
@@ -1,5 +1,4 @@
 angular.module('CapstoneSystem')
     .controller('WelcomeController', ['$scope', function ($scope) {
-        var height = $('.content').height();
-        $('.sidebar').height(height)​;
+
     }]);
