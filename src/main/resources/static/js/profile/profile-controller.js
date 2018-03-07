a944d7fbef0cc42a2d2964698fcfe0ff0df967e6
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-03-07 12:11:28 -0500

    [Issue #27] Made side navbar grey and take full page height

--- src/main/resources/static/js/profile/profile-controller.js
+++ src/main/resources/static/js/profile/profile-controller.js
@@ -1,3 +1,4 @@
 angular.module('CapstoneSystem')
     .controller('ProfileController', ['$scope', function ($scope) {
+        $('.sidenav').css("height",$(document).height() + "px");
     }]);
--- src/main/resources/static/js/projects/projects-controller.js
+++ src/main/resources/static/js/projects/projects-controller.js
@@ -14,4 +14,5 @@ angular.module('CapstoneSystem')
         $scope.froalaOptions = {
             toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo']
         }
+        $('.sidenav').css("height",$(document).height() + "px");
     }]);
