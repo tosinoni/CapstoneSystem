fb2c55c62a76be2f1882c36b237c722fb03bbb24
Tosin <tosin.oni@carleton.ca>
2018-03-07 20:13:03 -0500

    [Issue #31] added angular authentication of routes and fix side navbar

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,21 @@
 // Declare app level module which depends on filters, and services
-angular.module('CapstoneSystem', ['ngResource', 'ngRoute', 'routeStyles','froala'])
-    .config(function ($routeProvider, $locationProvider) {
+angular.module('CapstoneSystem', ['ngResource', 'ngRoute', 'routeStyles','froala', 'angular-jwt'])
+    .config(function ($routeProvider, $locationProvider, jwtOptionsProvider, $httpProvider) {
+
+        //configuring authentication
+        jwtOptionsProvider.config({
+            unauthenticatedRedirectPath: '/login',
+            authPrefix: '',
+            tokenGetter: ['Auth', function(Auth) {
+                if (Auth.isTokenExpired()) {
+                    return null;
+                }
+
+                return localStorage.getItem('token');
+            }]
+        });
+        $httpProvider.interceptors.push('jwtInterceptor');
+
         $routeProvider
             .when('/', {
                 templateUrl: 'views/home/home.html',
@@ -16,6 +31,7 @@ angular.module('CapstoneSystem', ['ngResource', 'ngRoute', 'routeStyles','froala
                 templateUrl: 'views/welcome/welcome.html',
                 controller: 'WelcomeController',
                 css: 'css/welcome/welcome.css',
+                requiresLogin: true
             })
             .when('/projects', {
                 templateUrl: 'views/projects/projects.html',
@@ -31,6 +47,7 @@ angular.module('CapstoneSystem', ['ngResource', 'ngRoute', 'routeStyles','froala
                 templateUrl: 'views/profile/profile.html',
                 controller: 'ProfileController',
                 css: 'css/profile/profile.css',
+                requiresLogin: true
             })
             .otherwise({
                 templateUrl: 'views/error/error.html'
@@ -41,6 +58,20 @@ angular.module('CapstoneSystem', ['ngResource', 'ngRoute', 'routeStyles','froala
                 requireBase: false
             });
     })
-    .run(function() {
+    .run(function($rootScope, Auth, authManager, $location) {
+        $rootScope.logout = function() {
+            Auth.logout();
+            location.reload();
+        }
 
+        //side nav height css changes
+        $('.sidenav').css("height",$(document).height() + "px");
+
+
+        $rootScope.$on("$locationChangeStart", function(event) {
+            $rootScope.isHomePage = $location.path() === "/";
+        });
+
+        authManager.checkAuthOnRefresh();
+        authManager.redirectWhenUnauthenticated();
     });
