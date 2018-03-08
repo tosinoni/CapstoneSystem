986315afd45ec1120ffc354a732f3075d1b9cd57
loushiano <ibrahimali.fawaz12@gmail.com>
2018-03-07 20:43:06 -0500

    Fozi (#30)
    * abstract class user
    
    * subclass prof that inherits from user
    
    * subclass Student that inherits from user
    
    * subclass Coordinator that inherits from Prof
    
    * class project contains all fields necessary for a project
    
    * class program contains all fields necessary for a program
    
    * class Room to represent where the presentation are going to be held
    
    * class project contains all fields necessary for a project
    
    * class Room to represent where the presentation are going to be held
    
    * class Time is a wrapper for the time of presentations
    
    *  error was in coordinator
    
    * fixed more issues found with travis that are not actually real issues
    
    * fixed more issues found with travis that are not actually real issues
    
    * only keeping user classes
    
    * added initial repositories
    
    * finished implementing initial authentication and authorization filters
    
    * a controller that handles the validation of login and signup
    
    * added mapping for sign up and login url that accept request bodies
    
    *  finished first part where the restfull controller added is test with validation and webUser annotations are tested as well
    
    *  fixed travis bug
    
    *  fixed travis bug
    
    * fixed all issues from review

--- src/main/java/com/carleton/CapstoneSystem/CapstoneSystemApplication.java
+++ src/main/java/com/carleton/CapstoneSystem/CapstoneSystemApplication.java
@@ -2,9 +2,16 @@ package com.carleton.CapstoneSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CapstoneSystemApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneSystemApplication.class, args);
