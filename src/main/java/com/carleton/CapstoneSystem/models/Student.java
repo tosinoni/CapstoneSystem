1f78825edfa6eba0bad2b6ac5f1bd68e95e2e649
alialsaaidi <alifawaz@cmail.carleton.ca>
2018-03-20 10:34:19 -0400

    impelemented a subclass of user called student, that will represent the student users

---
+++ src/main/java/com/carleton/CapstoneSystem/models/Student.java
@@ -0,0 +1,69 @@
+package com.carleton.CapstoneSystem.models;
+
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Inheritance
+public class Student extends WebUser{
+    @OneToOne(fetch = FetchType.EAGER)
+    private Project project;
+
+    @Column(name="program",nullable=true)
+    private Program program;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<Project> appliedProjects;
+
+    /**
+     *
+     * @return the program of this student
+     */
+    public Program getProgram() {
+        return program;
+    }
+
+    /**
+     *
+     * @param program to be set as the program of this student
+     */
+    public void setProgram(Program program) {
+        this.program = program;
+    }
+
+
+
+    /**
+     *
+     * @return the project of this student
+     */
+    public Project getProject() {
+        return project;
+    }
+
+    /**
+     *
+     * @param project to set the project of this student
+     */
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public void applyForProject(Project project){
+        appliedProjects.add(project);
+    }
+    public void removeAppliedProject(Project project){
+        appliedProjects.remove(project);
+    }
+
+
+
+
+
+
+}
