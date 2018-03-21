35af986c0956033d680b6f6a65bf28ee9524df24
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-03-20 20:57:49 -0400

    [Issue #58] Created modal to add students to project (#65)

--- src/main/resources/static/js/projects/projects-controller.js
+++ src/main/resources/static/js/projects/projects-controller.js
@@ -6,6 +6,10 @@ angular.module('CapstoneSystem')
         $('.programList').select2({
             placeholder: 'Select program(s)'
         });
+        $('.studentList').select2({
+            placeholder: 'Select student(s)'
+        });
+
         $('#projectsTable tbody').on('click', 'tr', function () {
             var data = table.row( this ).data();
             window.location.href = '/project';
