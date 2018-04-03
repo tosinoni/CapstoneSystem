851d6bd678deac553fa2d43089e55d51dd6af047
tosinoni <tosin.oni@carleton.ca>
2018-04-03 11:41:58 -0400

    [Issue #87] finalized the apply and cancel button (#89)
    * [Issue #87] finalized the apply and cancel button
    
    * [Issue #87] clean up

---
+++ src/main/resources/static/js/services/SwalService.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('CapstoneSystem')
+    .factory('SwalService', function() {
+
+        var deleteSwal = function(callback, confirmButtonText) {
+            swal({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: confirmButtonText
+            }).then(function(result) {
+                if (result) {
+                    callback();
+                }
+            }).catch(function(result) {});
+        };
+
+        return {
+            delete : deleteSwal
+        }
+    });
